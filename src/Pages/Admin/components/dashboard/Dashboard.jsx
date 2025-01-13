95689fa2f72e52a1b7b1cb8924b3f759cd307f02
fred011 <fmmphahle01@gmail.com>
2025-01-13 13:23:50 +0200

    13/01 upd 5

--- src/Pages/Admin/components/dashboard/Dashboard.jsx
+++ src/Pages/Admin/components/dashboard/Dashboard.jsx
@@ -1,20 +1,10 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from "react";
-
-import axios from "axios";
-import { Box } from "@mui/material";
-import { Typography } from "antd";
+import React from "react";
 
 const Dashboard = () => {
-  const [admin, setAdmin] = useState(null);
-
   return (
     <>
-      {admin && (
-        <Box>
-          <Typography.Title level={2}>Welcome {admin.name}</Typography.Title>
-        </Box>
-      )}
+      <h1>Admin Dash</h1>
     </>
   );
 };
