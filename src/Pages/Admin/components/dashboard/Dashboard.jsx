62ee6bcc8ee34a6c7a5412120dec3f0b7078c798
fred011 <fmmphahle01@gmail.com>
2025-01-13 13:54:12 +0200

    13/01 upd 5

--- src/Pages/Admin/components/dashboard/Dashboard.jsx
+++ src/Pages/Admin/components/dashboard/Dashboard.jsx
@@ -1,10 +1,46 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { baseAPI } from "../../../../environment";
+import { Box, Typography } from "@mui/material";
 
 const Dashboard = () => {
+  const [admin, setAdmin] = useState(null);
+  const fetchAdmin = () => {
+    // fetch admin data
+    axios
+      .get(`${baseAPI}/admin/fetch-admin`)
+      .then((res) => {
+        console.log(res);
+        setAdmin(res.data.admin);
+      })
+      .catch((e) => {
+        console.log("Error", e);
+      });
+  };
+
+  useEffect(() => {
+    fetchAdmin();
+  }, []);
+
   return (
     <>
       <h1>Admin Dash</h1>
+      {admin && (
+        <Box
+          sx={{
+            height: "500px",
+            width: "100%",
+            background: `url(/images/BackG.jpg)`,
+            backgroundSize: "cover",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography varient="h3">Welcome, {admin.name}</Typography>
+        </Box>
+      )}
     </>
   );
 };
