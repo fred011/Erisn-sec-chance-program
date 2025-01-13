443fc03d4d770bcd31d91347072c54324cee0fac
fred011 <fmmphahle01@gmail.com>
2025-01-13 17:11:12 +0200

    upd8

--- src/Pages/Admin/components/dashboard/Dashboard.jsx
+++ src/Pages/Admin/components/dashboard/Dashboard.jsx
@@ -1,10 +1,29 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Box, Typography } from "@mui/material";
 
 const Dashboard = () => {
+  const [adminName, setAdminName] = useState("");
+
+  useEffect(() => {
+    // Replace with your actual API endpoint
+    const fetchAdminName = async () => {
+      try {
+        const response = await fetch(
+          "https://erisn-api.onrender.com/api/admin/name"
+        );
+        const data = await response.json();
+        setAdminName(data.name);
+      } catch (error) {
+        console.error("Error fetching admin name:", error);
+      }
+    };
+
+    fetchAdminName();
+  }, []);
+
   return (
     <>
       <h1>Admin Dash</h1>
@@ -20,8 +39,8 @@ const Dashboard = () => {
           alignItems: "center",
         }}
       >
-        <Typography varient="h3">
-          Erisn Africa`s Student Management System
+        <Typography variant="h3" color="white">
+          Welcome Admin
         </Typography>
       </Box>
     </>
