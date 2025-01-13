55d7a4dd7e9a57ea2b924058f60c89d52b62a3ba
fred011 <fmmphahle01@gmail.com>
2025-01-13 15:41:46 +0200

    13/01 upd6

--- src/Pages/Admin/components/dashboard/Dashboard.jsx
+++ src/Pages/Admin/components/dashboard/Dashboard.jsx
@@ -5,42 +5,25 @@ import { baseAPI } from "../../../../environment";
 import { Box, Typography } from "@mui/material";
 
 const Dashboard = () => {
-  const [admin, setAdmin] = useState(null);
-  const fetchAdmin = () => {
-    // fetch admin data
-    axios
-      .get(`${baseAPI}/admin`)
-      .then((res) => {
-        console.log(res);
-        setAdmin(res.data.admin);
-      })
-      .catch((e) => {
-        console.log("Error", e);
-      });
-  };
-
-  useEffect(() => {
-    fetchAdmin();
-  }, []);
-
   return (
     <>
       <h1>Admin Dash</h1>
-      {admin && (
-        <Box
-          sx={{
-            height: "500px",
-            width: "100%",
-            background: `url(/images/BackG.jpg)`,
-            backgroundSize: "cover",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Typography varient="h3">Welcome, {admin.name}</Typography>
-        </Box>
-      )}
+
+      <Box
+        sx={{
+          height: "500px",
+          width: "100%",
+          background: `url(/images/BackG.jpg)`,
+          backgroundSize: "cover",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography varient="h3">
+          Erisn Africa`s Student Management System
+        </Typography>
+      </Box>
     </>
   );
 };
