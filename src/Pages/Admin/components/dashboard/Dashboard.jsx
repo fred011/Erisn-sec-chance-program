8e3f211e17d8b1ee71e6348d07c97a8d71acac37
fred011 <fmmphahle01@gmail.com>
2025-01-15 12:45:14 +0200

    15/01 upd 6

--- src/Pages/Admin/components/dashboard/Dashboard.jsx
+++ src/Pages/Admin/components/dashboard/Dashboard.jsx
@@ -1,27 +1,10 @@
 /* eslint-disable no-unused-vars */
 
 import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
+
 import { Box, Typography } from "@mui/material";
 
 const Dashboard = () => {
-  const [adminName, setAdminName] = useState("");
-
-  useEffect(() => {
-    const fetchAdminName = async () => {
-      try {
-        const response = await axios.get(
-          "https://erisn-api.onrender.com/api/admin/register"
-        );
-        setAdminName(response.data.name);
-      } catch (error) {
-        console.error("Error fetching admin name:", error);
-      }
-    };
-
-    fetchAdminName();
-  }, []);
   return (
     <>
       <Box
@@ -44,7 +27,7 @@ const Dashboard = () => {
           }}
         >
           <Typography variant="h3" color="lightgrey">
-            Welcome, {adminName}
+            Welcome, Admin
           </Typography>
 
           <Typography variant="h5" color="grey">
