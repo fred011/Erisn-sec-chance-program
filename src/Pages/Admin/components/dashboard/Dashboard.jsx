d40efd4cbaa2752cc445c5b76ef5febc9c3f380e
fred011 <fmmphahle01@gmail.com>
2025-01-16 17:01:26 +0200

    16/01

--- src/Pages/Admin/components/dashboard/Dashboard.jsx
+++ src/Pages/Admin/components/dashboard/Dashboard.jsx
@@ -3,85 +3,8 @@
 import React from "react";
 
 import { Box, Typography } from "@mui/material";
-import { useState } from "react";
-import { useEffect } from "react";
-import axios from "axios";
-import PropTypes from "prop-types"; // Import PropTypes
-import { baseAPI } from "../../../../environment";
-
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-
-// const Dashboard = () => {
-//   const [adminName, setAdminName] = useState("");
-
-//   useEffect(() => {
-//     const fetchAdminData = async () => {
-//       try {
-//         const response = await axios.get("/fetch-single");
-//         if (response.data.success) {
-//           setAdminName(response.data.admin.name);
-//         }
-//       } catch (error) {
-//         console.error("Error fetching admin data:", error);
-//       }
-//     };
-
-//     fetchAdminData();
-//   }, []);
-
-//   return (
-//     <>
-//       <Box
-//         sx={{
-//           height: "270px",
-//           width: "100%",
-//           background: `url(/images/BackG.jpg)`,
-//           backgroundSize: "cover",
-//           display: "flex",
-//           justifyContent: "center",
-//           alignItems: "center",
-//         }}
-//       >
-//         <Box
-//           sx={{
-//             textAlign: "center",
-//             display: "flex",
-//             flexDirection: "column",
-//             alignItems: "center",
-//           }}
-//         >
-//           <Typography variant="h3" color="lightgrey">
-//             Welcome, {adminName}
-//           </Typography>
-
-//           <Typography variant="h5" color="grey">
-//             To Erisn Africa`s Student Management System
-//           </Typography>
-//         </Box>
-//       </Box>
-//     </>
-//   );
-// };
 
 const Dashboard = () => {
-  const [adminName, setAdminName] = useState("");
-
-  const fetchAdminName = async () => {
-    axios
-      .get(`${baseAPI}/admin/name`)
-      .then((res) => {
-        console.log("Admin name fetched successfully:", res.data.name);
-        setAdminName(res.data.name);
-      })
-      .catch((err) => {
-        console.log("Error fetching admin name:", err);
-      });
-  };
-  useEffect(() => {
-    fetchAdminName();
-  }, []);
-
   return (
     <>
       <Box
@@ -103,15 +26,9 @@ const Dashboard = () => {
             alignItems: "center",
           }}
         >
-          {adminName ? (
-            <Typography variant="h3" color="lightgrey">
-              Welcome, {adminName}!
-            </Typography>
-          ) : (
-            <Typography variant="h3" color="lightgrey">
-              Welcome, Admin!
-            </Typography>
-          )}
+          <Typography variant="h3" color="lightgrey">
+            Welcome, Admin
+          </Typography>
 
           <Typography variant="h5" color="grey">
             To Erisn Africa`s Student Management System
@@ -123,40 +40,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-{
-  /* <Box
-        component={"div"}
-        sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
-      >
-        {subjects &&
-          subjects.map((x) => {
-            return (
-              <Paper key={x._id} sx={{ m: 2, p: 2 }}>
-                <Box component={"div"}>
-                  <Typography variant="h4">
-                    Subject: {x.subject_name} [{x.subject_codename}]
-                  </Typography>
-                </Box>
-                <Box component={"div"}>
-                  <Button
-                    onClick={() => {
-                      handleEdit(x._id, x.subject_name, x.subject_codename);
-                    }}
-                  >
-                    <EditIcon />
-                  </Button>
-                  <Button
-                    onClick={() => {
-                      handleDelete(x._id);
-                    }}
-                    sx={{ color: "red" }}
-                  >
-                    <DeleteIcon />
-                  </Button>
-                </Box>
-              </Paper>
-            );
-          })}
-      </Box> */
-}
