36be759dd88ce2a4ab4d9b7fd3e1540a46b8b6ce
fred011 <fmmphahle01@gmail.com>
2025-01-17 09:29:04 +0200

    17/01

--- src/Pages/Admin/components/schedule/Schedule.jsx
+++ src/Pages/Admin/components/schedule/Schedule.jsx
@@ -1,11 +1,38 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const Schedule = () => {
+const localizer = momentLocalizer(moment);
+
+export default function Schedule() {
+  const date = new Date();
+  const myEventsList = [
+    {
+      id: 1,
+      title: "Subject: History, Teacher: Harry",
+      start: date.setHours(11, 30),
+      end: date.setHours(14, 30),
+    },
+    {
+      id: 1,
+      title: "Subject: English, Teacher: Fred",
+      start: date.setHours(15, 30),
+      end: date.setHours(18, 30),
+    },
+  ];
+
   return (
     <>
       <h1>Schedule</h1>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
     </>
   );
-};
-
-export default Schedule;
+}
