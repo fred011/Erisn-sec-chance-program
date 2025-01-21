0f78372b302ef0dc0a266b2aa8f5c053b1df07bc
fred011 <fmmphahle01@gmail.com>
2025-01-21 18:23:20 +0200

    21/01

--- src/Pages/Admin/components/attendence/Attendee.jsx
+++ src/Pages/Admin/components/attendence/Attendee.jsx
@@ -1,11 +1,12 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useEffect } from "react";
 
 export default function Attendee({ classId }) {
   useEffect(() => {
     console.log("CLASS ID:", classId);
-  }, []);
+  }, [classId]);
   return (
     <>
       <h1>Attendee</h1>
