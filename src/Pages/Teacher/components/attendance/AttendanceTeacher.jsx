d4dd46ac61c7da4ec3db717cc3e90b88e35dcb90
fred011 <fmmphahle01@gmail.com>
2025-01-30 16:03:34 +0200

    30/01

--- src/Pages/Teacher/components/attendance/AttendanceTeacher.jsx
+++ src/Pages/Teacher/components/attendance/AttendanceTeacher.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { baseAPI } from "../../../../environment";
 
 const AttendanceTeacher = () => {
+  const [classes, setClasses] = useState([]);
+
+  const fetchAttendeeClass = async () => {
+    const token = localStorage.getItem("token"); // Retrieve token from localStorage or context
+
+    try {
+      const response = await axios.get(`${baseAPI}/class/attende`, {
+        headers: {
+          Authorization: `Bearer ${token}`, // Include token in the request header
+        },
+      });
+      console.log("Attendee class Res:", response);
+    } catch (error) {
+      console.log("Error in fetching all Teacher Attendee Data", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAttendeeClass();
+  }, []);
+
   return (
     <>
       <h1>Teacher Attendance</h1>
