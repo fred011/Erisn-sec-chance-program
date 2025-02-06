59ee08a1221fa9e902bb42f41869433ae4054616
fred011 <fmmphahle01@gmail.com>
2025-02-06 11:22:01 +0200

    06/02

--- src/Components/yupSchema/registerSchema.js
+++ src/Components/yupSchema/registerSchema.js
@@ -1,8 +1,13 @@
+/* eslint-disable no-control-regex */
 import * as Yup from "yup";
+const emailRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export const registerSchema = Yup.object({
   name: Yup.string().required("Name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email format")
+    .required("Email is required"),
   password: Yup.string()
     .min(6, "Password must be at least 6 characters")
     .required("Password is required"),
--- src/Components/yupSchema/studentSchema.js
+++ src/Components/yupSchema/studentSchema.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-control-regex */
 import * as Yup from "yup";
+const emailRegex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 export const studentSchema = Yup.object({
   name: Yup.string()
     .min(3, "Student name must contain 3 characters")
     .required("Name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email format")
+    .required("Email is required"),
   student_class: Yup.string().required("Student class is required"),
   age: Yup.string().required("Age is required"),
   gender: Yup.string().required("Gender is required"),
@@ -26,7 +31,9 @@ export const studentEditSchema = Yup.object({
   name: Yup.string()
     .min(3, "Student name must contain 3 characters")
     .required("Name is required"),
-  email: Yup.string().email("Invalid email").required("Email is required"),
+  email: Yup.string()
+    .matches(emailRegex, "Invalid email format")
+    .required("Email is required"),
   student_class: Yup.string().required("Student class is required"),
   age: Yup.string().required("Age is required"),
   gender: Yup.string().required("Gender is required"),
